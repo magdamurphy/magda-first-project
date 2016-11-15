98ac330de529a25a2c838e3cc689c571cebc85fd
magda <magdalena2019@gmail.com>
2016-11-15 21:53:05 +0000

    first method

--- magda.rb
+++ magda.rb
@@ -1,2 +1,10 @@
-puts "hello world!"
-puts "zabka is the best"
+def isThisMyFirstMethod(isTrue)
+  if isTrue.to_i == 9
+    puts "Yes"
+  else
+    puts "No"
+  end
+end
+
+input = gets.chomp
+isThisMyFirstMethod(input)
